fd15d38ff7221e90d8a60830d37d21ac1d36ae6e
ManuelonGithub <manuelonemail@gmail.com>
2023-04-10 21:02:44 -0400

    new example shell command added

--- src/tiva_shell.c
+++ src/tiva_shell.c
@@ -12,6 +12,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "driverlib/debug.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/rom_map.h"
@@ -28,9 +29,9 @@ void init()
 #ifdef CLK_80M
 	// Theory of Operation:
 	//	The PLL clock will run at 400MHz when the 16MHz external crystal is connected to it
-	//	When set the PLL division to be 2.5
-	//	And then by default this value gets divided by 2
-	// 	(400M / 2.5) / 2 = 80M
+	// 	then by default this value gets divided by 2
+	//	and then we set the PLL division to be 2.5
+	// 	(400M / 2) / 2.5 = 80M
 	MAP_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ);
 #else
 	MAP_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_XTAL_16MHZ);
@@ -38,6 +39,9 @@ void init()
 
 	stdio_init();
 }
+
+/**************************************************************************************************/
+/**************************************************************************************************/
 
 int main(void)
 {
@@ -50,19 +54,48 @@ int main(void)
 	}
 }
 
-void say_hello(int argc, char** argv)
+/**************************************************************************************************/
+/**************************************************************************************************/
+
+static void hello(int argc, char** argv)
 {
 	stdio_print("HELLO :D!\n");
 }
 
-void say_goodbye(int argc, char** argv)
+static void goodbye(int argc, char** argv)
 {
 	stdio_print("Goodbye :(...\n");
 }
 
-REGISTER_CMD("hello", say_hello, "says hello");
-REGISTER_CMD("goodbye", say_goodbye, "says goodbye");
+REGISTER_CMD("hello", hello, "says hello");
+REGISTER_CMD("goodbye", goodbye, "says goodbye");
 
+static void sum(int argc, char** argv)
+{
+	int sum = 0;
+	for (int i = 0; i < argc; i++) {
+		int val = 0;
+		char* check;
+		
+		val = strtol(argv[i], &check, 10);
+		if (argv[i] == check) {
+			goto usage;
+		}
+
+		sum +=val;
+	}
+
+	stdio_printf("sum: %d\n", sum);
+	return;
+
+usage:
+	stdio_print("usage: sum ## ## ## ... all signed 32-bit decimal");
+}
+
+REGISTER_CMD("sum", sum, "sums signed 32-bit decimal numbers. Limited by max arg count");
+
+/**************************************************************************************************/
+/**************************************************************************************************/
 
 // The error routine that is called if the driver library encounters an error.
 #ifdef DEBUG
